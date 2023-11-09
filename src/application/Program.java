bdef8e1852535ed95a7091506e3ecab3976c62b0
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-09 13:06:37 -0300

    Update Program, criada instanciação de UsedProduct

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,26 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Product;
+import entities.UsedProduct;
 
 public class Program {
 	
-	public static void main(String[]args) {
+	public static void main(String[]args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Product> list = new ArrayList<>();
 		
 		System.out.print("Enter the number of products: ");
 		int n = sc.nextInt();
@@ -30,7 +40,14 @@ public class Program {
 			
 			if(aux.toUpperCase().equals("C")) {
 				Product product = new Product(name, price);
-			} 
+				list.add(product);
+			} else if(aux.toUpperCase().equals("U")) {
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date date = sdf.parse(sc.next());
+				
+				Product product = new UsedProduct(name, price, date);
+				list.add(product);
+			}
 		}
 		
 		sc.close();
