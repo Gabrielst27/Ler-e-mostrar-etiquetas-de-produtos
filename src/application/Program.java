a179a62fb03ed013a4438892984766b6de82b071
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-09 12:05:27 -0300

    Criado laço de instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,18 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		sc.nextLine();
 		
+		for(int i = 1; i <= n; i++) {
+			System.out.printf("\nProduct #%d data\n", i);
+			System.out.print("Common, used or imported? (c/u/i): ");
+			String aux = sc.nextLine();
+			
+			
+			
+		}
 		
 		sc.close();
 		
